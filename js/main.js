32846dff701b6a144e88e7af91577a836de1bbf3
Your Name <usman.kamailaev@yandex.ru>
2020-08-24 17:51:30 +0300

    team

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ openingBtn.addEventListener("click", e => {
     e.preventDefault();  
     mobalMenu.classList.remove("mobal-menu--opened");
   });
+
+
+const findBlockByAlias = (alias) => {
+  return $(".reviews__item").filter((ndx, item) => {
+    return $(item).attr("data-linked-with") === alias;
+  });
+};
+
+  $(".ineractive-avatar-link").click((e) => {
+    e.preventDefault();
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-open");
+    const itemToShow = findBlockByAlias(target);``
+    const curItem = $this.closest(".reviews__switcher-item");
+    itemToShow.addClass("active").siblings().removeClass("active");
+    curItem.addClass("ineractive-avatar-active").siblings().removeClass("ineractive-avatar-active");
+  })
