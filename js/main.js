911fbfa310e5d8b307fa529cfe86e8c79fba4159
Your Name <usman.kamailaev@yandex.ru>
2020-09-02 20:34:55 +0300

    Иeats

--- js/main.js
+++ js/main.js
@@ -283,15 +283,15 @@ $("[data-scroll-to]").click(e => {
 })
 
 //swipe
-$("body").swipe( {
-  swipe:function(
-    event,
-    direction 
-    ) {
-    const scroller = viewportScroller();
-    let scrollDirection = "";
-    if (direction === "up") scrollDirection = "next";
-    if (direction === "down") scrollDirection = "prev";
-    scroller[scrollDirection]();
-     }
-});
+// $("body").swipe( {
+//   swipe:function(
+//     event,
+//     direction 
+//     ) {
+//     const scroller = viewportScroller();
+//     let scrollDirection = "";
+//     if (direction === "up") scrollDirection = "next";
+//     if (direction === "down") scrollDirection = "prev";
+//     scroller[scrollDirection]();
+//      }
+// });
