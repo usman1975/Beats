da40b50aa47248db52ecf108462b296cda6f9792
Your Name <usman.kamailaev@yandex.ru>
2020-09-02 19:51:14 +0300

    beats

--- js/accordion.js
+++ js/accordion.js
@@ -12,4 +12,59 @@ sizesItem.forEach(item => {
         }  
     })
 }) 
+// скрипты для видео
+const containerVideo = document.querySelector('.video');
+const video = containerVideo.querySelector('video');
+const playpause = containerVideo.querySelector('.video__playpause');
+const play = containerVideo.querySelector('.video__play');
+const controls = containerVideo.querySelector('.video__controls');
+const total = containerVideo.querySelector('.video__total');
+const progress = containerVideo.querySelector('.video__current');
+const dynamic = containerVideo.querySelector('.video__volume-control');
+const volume = containerVideo.querySelector('.video__volume-progress');
+const volumeProgress = volume.firstElementChild;
 
+playpause.addEventListener('click', togglePlay);
+play.addEventListener('click', togglePlay);
+video.addEventListener('play', playPause);
+video.addEventListener('pause', playPause);
+total.addEventListener('click', setCurrentTime);
+video.addEventListener('timeupdate', timeUpdate);
+dynamic.addEventListener('click', mute);
+volume.addEventListener('click', setVolume);
+
+function setVolume(e) {
+    volumeProgress.style.width = `${e.offsetX}px`;
+    console.log(e.offsetX / volume.clientWidth)
+    video.volume = e.offsetX / volume.clientWidth;
+    volumeProgress.style.background = "#E01F3D";
+}
+
+function mute(e) {
+    dynamic.classList.toggle('muted');
+    console.log(video.muted)
+    video.muted = !video.muted;
+    volumeProgress.style.width = 0;
+}
+
+function playPause() {
+    controls.classList.toggle('paused');
+}
+
+function togglePlay() {
+    video.paused ?  video.play() : video.pause();
+}
+
+function setCurrentTime(e) {
+    const offsetX = e.offsetX / total.clientWidth;
+    console.log(offsetX * video.duration)
+    video.currentTime = offsetX * video.duration;
+}
+
+function timeUpdate() {
+    console.log('up')
+    const progressTime = video.currentTime / video.duration;
+
+    progress.style.width = `${progressTime * total.clientWidth}px`;
+    progress.style.background = "#E01F3D";
+}
--- js/main.js
+++ js/main.js
@@ -73,10 +73,225 @@ const closeEveryItem = container => {
     slider.goToNextSlide();
   });
 
+//отправка формы
+
+const validateFields = (form, fieldsArray) => {
+  fieldsArray.forEach(field => {
+    field.removeClass('input-error');
+    if (field.val().trim() === "") {
+      field.addClass('input-error');
+    }
+  })
+
+const errorFields = form.find(".input-error");
+
+   return  errorFields.length === 0;
+}
+
 $('.form').submit(e => {
   e.preventDefault();
-  $.fancybox.open({
-    src: "#modal",
-    type: "inline"
-  })
+   
+  const form = $(e.currentTarget);
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']");
+  const to = form.find("[name='to']");
+  const modal = $("#modal");
+  const content = modal.find(".modal__content");
+  
+
+  modal.removeClass("error-modal");
+  const isValid = validateFields(form, [name, phone, comment, to]);
+  
+if (isValid) {
+  $.ajax({
+    url: "https://webdev-api.loftschool.com/sendmail",
+    method: "POST",
+    data: {
+      name: name.val(),
+      phone: phone.val(),
+      comment: comment.val(),
+      to: to.val()
+    },
+    success: data => {
+      content.text(data.message);
+      console.log(data);
+      $.fancybox.open({
+        src: "#modal",
+        type: "inline"
+      });
+    },
+
+    error: data => {
+      const message = data.responseJSON.message;
+      content.text(message);
+      modal.addClass("error-modal");
+      
+      $.fancybox.open({
+        src: "#modal",
+        type: "inline"
+      });
+    }
+  });
+}
+});
+
+
+
+$(".app-close-modal").click(e => {
+  e.preventDefault();
+  $.fancybox.close();
+})
+
+
+//подключение яндекс карты
+
+let myMap;
+const init = () => {
+    myMap = new ymaps.Map("map", {
+      center: [55.753215, 37.622504],
+      zoom: 13,
+      controls: []
+    });
+
+    let coords = [
+      [55.760270, 37.581850],
+      [55.76027, 37.647147],
+      [55.746258, 37.585098],
+      [55.750412, 37.602934],
+    ],
+    myCollection = new ymaps.GeoObjectCollection({}, {
+      draggable: false,
+      iconLayout: 'default#image',
+      iconImageHref: './img/icons/marker.svg',
+      iconImageSize: [46, 57],
+      iconImageOffset: [-35, -52]
+    });
+  
+  for (let i = 0; i < coords.length; i++) {
+    myCollection.add(new ymaps.Placemark(coords[i]));
+  }
+  
+  myMap.geoObjects.add(myCollection);
+  
+  myMap.behaviors.disable('scrollZoom');
+}
+
+ymaps.ready(init);
+
+
+//ops
+
+const sections = $("section");
+const display = $(".maincontent");
+
+let inScroll = false;
+
+sections.first().addClass("active");
+
+const performTransition = sectionEq => {
+
+  if (inScroll === false) {
+    inScroll = true;
+    const position = sectionEq * -100;
+
+    const currentSection = sections.eq(sectionEq);
+    const menuTheme = currentSection.attr("data-sidemenu-theme");
+    const sideMenu = $(".fixed-menu ");
+
+    if (menuTheme === "black") {
+      sideMenu.addClass("fixed-menu--shadowed");
+    } else {
+      sideMenu.removeClass("fixed-menu--shadowed");
+    }
+
+
+    display.css({
+      transform: `translateY(${position}%)`
+    });
+  
+    sections.eq(sectionEq).addClass("active").siblings().removeClass("active");
+   
+
+    setTimeout( () => {
+      inScroll = false;
+      sideMenu.find(".fixed-menu__item").eq(sectionEq).addClass("fixed-menu__item--active").siblings().removeClass("fixed-menu__item--active");
+    }, 1300 );
+  }
+ 
+};
+
+const scrollViewport = direction => {
+
+  const activeSection = sections.filter(".active");
+  const nextSection = activeSection.next();
+  const prevSection = activeSection.prev();
+  if (direction === "next" && nextSection.length) {
+    performTransition(nextSection.index());
+  }
+
+  if (direction === "prev" && prevSection.length) {
+    performTransition(prevSection.index());
+  }
+};
+
+
+
+$(window).on("wheel", e =>  {
+  const deltaY = e.originalEvent.deltaY;
+  if (deltaY > 0) {
+    scrollViewport("next");
+  };
+  if (deltaY < 0) {
+    scrollViewport("prev");
+  };
+});
+
+$(window).on("keydown", e => {
+  const tagName = e.target.tagName.toLowerCase();
+   console.log(e.keyCode);
+   if (tagName !== "input" && tagName !== "textarea") {
+    switch(e.keyCode) {
+      case 38:
+        scrollViewport("prev");
+        break;
+      case 40:
+       scrollViewport("next");
+       break; 
+    }
+   }
+  
+})
+
+//links
+
+$("[data-scroll-to]").click(e => {
+  e.preventDefault();
+  const $this = $(e.currentTarget);
+  const target = $this.attr("data-scroll-to");
+  console.log(target);
+  const regSection = $(`[data-section-id=${target}]`);
+  console.log(regSection.index());
+  if (regSection.index() !== 0 && regSection.index() !== 9) {
+    performTransition(regSection.index());
+  } else if(regSection.index() == 9) {
+    performTransition(8);
+  } else {
+    performTransition(0);
+  }
+
+})
+
+//swipe
+$("body").swipe( {
+  swipe:function(
+    event,
+    direction 
+    ) {
+    const scroller = viewportScroller();
+    let scrollDirection = "";
+    if (direction === "up") scrollDirection = "next";
+    if (direction === "down") scrollDirection = "prev";
+    scroller[scrollDirection]();
+     }
 });
